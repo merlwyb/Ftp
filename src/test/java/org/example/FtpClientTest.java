71abf2d2bac50edc56a04d0ce926eff38a5e6f5a
Abc <wybunetri@gmail.com>
2023-01-14 16:23:30 +0500

    JARs, README, TESTS

--- src/test/java/org/example/FtpClientTest.java
+++ src/test/java/org/example/FtpClientTest.java
@@ -14,6 +14,11 @@ public class FtpClientTest {
         Config.PASSWORD = args[2];
         Config.SERVER = args[3];
         Config.TESTFILENAME = args[4];
+//        String suiteFileName = "src/test/java/resources/testing.xml";
+//        Config.USERNAME = "ftp";
+//        Config.PASSWORD = "ftp";
+//        Config.SERVER = "192.168.0.105";
+//        Config.TESTFILENAME = "Test.json";
 
         List<String> suites = Lists.newArrayList(suiteFileName);
         testNG.setTestSuites(suites);
